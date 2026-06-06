f2092bbad09a0e31c42f4ebd6a334b7d57d26b93
Steve Tardell <stardell@gmail.com>
2026-06-05 21:14:47 -0700

    Port Phase 3: Wire MainActivity to MainScreen

--- app/src/main/java/com/stardell/parpilotai/MainActivity.kt
+++ app/src/main/java/com/stardell/parpilotai/MainActivity.kt
@@ -4,44 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.stardell.parpilotai.ui.screens.MainScreen
 import com.stardell.parpilotai.ui.theme.ParPilotAITheme
+import com.stardell.parpilotai.viewmodel.GolferViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: GolferViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ParPilotAITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            val appTheme by viewModel.themePreference.collectAsState()
+            val appStyleTheme by viewModel.appStyleTheme.collectAsState()
+            
+            ParPilotAITheme(
+                appTheme = appTheme,
+                appStyleTheme = appStyleTheme
+            ) {
+                MainScreen(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ParPilotAITheme {
-        Greeting("Android")
     }
 }
